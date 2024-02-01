f644234432cd3662852e3977dc2070c2018a00f4
Khalif Gaffarezka <kgaffarezka@gmail.com>
2024-02-01 15:12:36 +0700

    Comment tags component

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -15,7 +15,7 @@ function Article(props) {
     <>
       <h3>{props.title}</h3>
       <small>
-        Date : {props.date}, tags : {props.tags.join(",")}
+        Date : {props.date},{/* tags : {props.tags.join(",")} */}
         {/* {props.isNew ? "--Baru!!" : "lama"} */}
         {/* {props.isNew && "--Baru!!"} */}
         <ArticleStatus isNew={props.isNew} />
