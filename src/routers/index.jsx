a1bdcebab10082d2c2faa81e596c9ea3a311f0d5
Khalif Gaffarezka <kgaffarezka@gmail.com>
2024-02-02 13:29:22 +0700

    Add Error Page

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -6,12 +6,14 @@ import Blog from "../pages/blogs";
 import Post from "../pages/blogs/_id";
 import About from "../pages/about";
 
+import ErrorPage from "../components/ErrorPage";
 import { posts, postById } from "../apis/loaders";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
